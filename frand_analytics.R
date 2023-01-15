ab6f79f2dbdb73e48e3b3f5ca450044c21a67a1f
Marcos de Morais Silva <91103250+marcosmorais94@users.noreply.github.com>
2023-01-15 13:50:33 -0300

    Add files via upload

--- frand_analytics.R
+++ frand_analytics.R
@@ -7,6 +7,10 @@
 setwd('C:/python_scripts/fraud')
 getwd()
 
+# Carga dos pacote Caret ####
+
+library(caret) 
+
 # Carga dos datasets ####
 
 # Carga do dataset 1 e 2
@@ -16,15 +20,50 @@ bd <- read.csv('bd_fraudPython.csv') #1.000.000 registros
 View(bd)
 str(bd)
 
-
 # Pré-Processamento ####
-# Nesta etapa será removido registros nulos de algumas variáveis e
-# a normalização dos dados
+# Algumas variáveis estão com num, mas são catergorias
+# Exemplo de algumas abaixo
+sapply(X = bd['income'], FUN = 'unique')
+sapply(X = bd['month'], FUN = 'unique')
 
-#workclass, occupation e native country possuem valores como ?
-sum(ifelse(bd$workclass == ' ?', 1, 0)) #2.799 registros
-sum(ifelse(bd$occupation == ' ?', 1, 0)) #2.809 registros
-sum(ifelse(bd$native.country == ' ?', 1, 0)) #857 registros
+# Converte variáveis em tipo fator
+bd['income'] <- as.character(bd$income)
+bd['customer_age'] <- as.character(bd$customer_age)
+bd['month'] <- as.character(bd$month)
 
-# Remoção variável com mais registros inválidos
-bd_modelo <- bd_modelo[bd_modelo$occupation != ' ?',]
+# Confere os tipos de dados novamente
+str(bd)
+
+# Função para normalização dos dados
+bd[c('name_email_similarity','current_address_months_count','days_since_request'              ,'intended_balcon_amount','zip_count_4w', 'velocity_6h', 'bank_branch_count_8w',
+     'credit_risk_score','session_length_in_minutes')] <- 
+  scale(
+    bd[c('name_email_similarity','current_address_months_count','days_since_request'          ,'intended_balcon_amount','zip_count_4w', 'velocity_6h', 'bank_branch_count_8w',
+     'credit_risk_score','session_length_in_minutes')])
+
+# Varíavel Dummy ####
+
+# Neste etapa vamos separar a variável preditora (target) do dataset
+# A função exige que seja feita dessa forma, como se fosse um treino de modelo
+
+# Separa a variável target
+bd_dmy <- bd[,2:26]
+
+# Criação de variáveis dummy com todos os atributos do dataset
+# Função considera apenas os numéricos
+dmy <- dummyVars(" ~ .", data = bd_dmy, fullRank = T)
+
+# Cria dataset unindo a variável target com os demais atributos
+dat_modelo <- data.frame(predict(dmy, newdata = bd_dmy))
+
+# Dataset com a variável dummy para os atributos categóricos
+View(dat_modelo)
+
+# Inclusão da variável target no dataset
+bd_final <- dat_modelo
+bd_final['fraud_bool'] <- bd[,1]
+View(bd_final)
+str(bd_final)
+
+# Export csv com os dados
+write.csv2(bd_final, 'bd_final.csv', row.names = FALSE)
